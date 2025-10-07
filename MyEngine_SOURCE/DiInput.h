74ba10a720e75b3f4c2cdfb68ca00a99d77a7447
Yookpo <131447294+Yookpo@users.noreply.github.com>
2025-10-07 20:10:02 +0900

    Input 클래스 리팩토링

--- MyEngine_SOURCE/DiInput.h
+++ MyEngine_SOURCE/DiInput.h
@@ -16,6 +16,7 @@ namespace My
 		Q, W, E, R, T, Y, U, I, O, P,
 		A, S, D, F, G, H, J, K, L,
 		Z, X, C, V, B, N, M,
+		Left, Right, Down, Up,
 		End,
 	};
 
@@ -36,9 +37,15 @@ namespace My
 		static bool GetKeyUp(eKeyCode code) { return mKeys[(UINT)code].state == eKeyState::Up; }
 		static bool GetKey(eKeyCode code) { return mKeys[(UINT)code].state == eKeyState::Pressed; }
 
+	private:
+		static void CreateKeys();
+		static void UpdateKeys();
+		static void UpdateKey(Input::Key& key);
+		static bool IsKeyDown(eKeyCode code);
+		static void UpdateKeyDown(Input::Key& key);
+		static void UpdateKeyUp(Input::Key& key);
 
 	private:
-		//eKeyState mState[];
 		static std::vector<Key> mKeys;
 	};
 }
